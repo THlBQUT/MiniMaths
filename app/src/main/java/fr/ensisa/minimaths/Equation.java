b139841086ff7633ad22729825dc4dbd4c864b4a
Thibaut <hertzogthibaut@gmail.com>
2022-06-08 08:13:19 +0200

    Ajout classe equation

---
+++ app/src/main/java/fr/ensisa/minimaths/Equation.java
@@ -0,0 +1,19 @@
+package fr.ensisa.minimaths;
+
+public class Equation {
+
+    private enum Niveau{FACILE,MOYEN,DIFFICILE}
+
+    public Equation(Niveau niveau){
+        switch (niveau){
+            case FACILE:
+                break;
+            case MOYEN:
+                break;
+            case DIFFICILE:
+                break;
+            default:
+                break;
+        }
+    }
+}
--- app/src/main/java/fr/ensisa/minimaths/soloGameList.java
+++ app/src/main/java/fr/ensisa/minimaths/soloGameList.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class soloGameList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solo_game_list);
+    }
+
+    public void returnHome(View v){
         setContentView(R.layout.activity_main);
     }
 }
