043585a07800feeab2ff7a7d6c6e2c5a914f213c
Thibaut <hertzogthibaut@gmail.com>
2022-06-10 09:52:00 +0200

    Ajout ecran chargement + suppression Action Bar

---
+++ app/src/main/java/fr/ensisa/minimaths/startup_activity.java
@@ -0,0 +1,27 @@
+package fr.ensisa.minimaths;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.view.View;
+
+public class startup_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startup);
+        Intent intent = new Intent(this, MainActivity.class);
+        Runnable waiting = new Runnable() {
+            @Override
+            public void run() {
+                SystemClock.sleep(2500);
+                startActivity(intent);
+            }
+        };
+        Thread thread1 = new Thread(waiting);
+        thread1.start();
+    }
+}
