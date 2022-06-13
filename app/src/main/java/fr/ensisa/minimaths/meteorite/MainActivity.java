9aab20db0044b9906275638f340fbbe924d6ed0e
lilmo49 <85099974+lilmo49@users.noreply.github.com>
2022-06-13 11:43:15 +0200

    jeu_meteorite

--- app/src/main/java/fr/ensisa/minimaths/meteorite/MainActivity.java
+++ app/src/main/java/fr/ensisa/minimaths/meteorite/MainActivity.java
@@ -1,13 +1,25 @@
 package fr.ensisa.minimaths.meteorite;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set fullscreen
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Set No Title
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        this.setContentView(new GameSurface(this));
     }
+
 }
