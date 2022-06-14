e7bb152cd397581b2698d171ef17f5f50ac1474b
Bastien Cron <flavicron@gmail.com>
2022-06-14 15:20:03 +0200

    ajout des vibrations et des variables de score pour lazerbattlemenu et quizmenu

--- app/src/main/java/fr/ensisa/minimaths/SoloGameList.java
+++ app/src/main/java/fr/ensisa/minimaths/SoloGameList.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -19,26 +22,35 @@ public class SoloGameList extends AppCompatActivity {
     private Animation animSlideIn2, animZoomIn5, animZoomIn6, animZoomIn7, animZoomIn8;
     private CardView header2;
     private RelativeLayout btn5, btn6, btn7, btn8;
+    private Vibrator vibrator;
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solo_game_list);
+
+        preferences = getSharedPreferences("SHARED_PREF_MAIN", MODE_PRIVATE);
+        vibrator = (Vibrator) this.getSystemService(VIBRATOR_SERVICE);
+
         header2 = findViewById(R.id.header_sologamelist);
         btn5 = findViewById(R.id.solo_btn1);
         btn6 = findViewById(R.id.solo_btn2);
         btn7 = findViewById(R.id.solo_btn3);
         btn8 = findViewById(R.id.solo_btn4);
+
         animSlideIn2 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_down);
-        animSlideIn2.setStartOffset(300);
         animZoomIn5 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
-        animZoomIn5.setStartOffset(600);
         animZoomIn6 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
-        animZoomIn6.setStartOffset(800);
         animZoomIn7 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
-        animZoomIn7.setStartOffset(1000);
         animZoomIn8 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
+
+        animSlideIn2.setStartOffset(300);
+        animZoomIn5.setStartOffset(600);
+        animZoomIn6.setStartOffset(800);
+        animZoomIn7.setStartOffset(1000);
         animZoomIn8.setStartOffset(1200);
+
         header2.startAnimation(animSlideIn2);
         btn5.startAnimation(animZoomIn5);
         btn6.startAnimation(animZoomIn6);
@@ -48,54 +60,76 @@ public class SoloGameList extends AppCompatActivity {
 
     public void backButton(View v){
         onBackPressed();
+        if (preferences.getBoolean("SHARED_PREF_MAIN_VIBRATION", true))
+            vibrator.vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE));
     }
 
     public void returnHome(View v){
         setContentView(R.layout.activity_main);
+        if (preferences.getBoolean("SHARED_PREF_MAIN_VIBRATION", true))
+            vibrator.vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE));
     }
 
     public void goToHome(View v){
         Intent home = new Intent(this, MainActivity.class);
         startActivity(home);
         overridePendingTransition(0, android.R.anim.slide_out_right);
+        if (preferences.getBoolean("SHARED_PREF_MAIN_VIBRATION", true))
+            vibrator.vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE));
     }
 
     public void goToSettings(View v){
         Intent settingsIntent = new Intent(this, Settings.class);
         startActivity(settingsIntent);
+        if (preferences.getBoolean("SHARED_PREF_MAIN_VIBRATION", true))
+            vibrator.vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE));
     }
 
     public void goToLazerBattle(View v){
         Intent activityLazer = new Intent(this, LazerBattleMenu.class);
         startActivity(activityLazer);
+        if (preferences.getBoolean("SHARED_PREF_MAIN_VIBRATION", true))
+            vibrator.vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE));
     }
 
     public void goToMeteorite(View v){
         Intent activityMeteorite = new Intent(this, MeteorActivity.class);
         startActivity(activityMeteorite);
+        if (preferences.getBoolean("SHARED_PREF_MAIN_VIBRATION", true))
+            vibrator.vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE));
     }
 
     public void goToQuizz(View v){
         Intent activityQuizz = new Intent(this, Quizz_Menu.class);
         startActivity(activityQuizz);
+        if (preferences.getBoolean("SHARED_PREF_MAIN_VIBRATION", true))
+            vibrator.vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE));
     }
     public void goToLabyrinthTip(View v) {
         Toast viewToast = Toast.makeText(this, "Retrouvez les nombres et opérateurs perdus dans le labyrinthe", Toast.LENGTH_LONG);
         viewToast.show();
+        if (preferences.getBoolean("SHARED_PREF_MAIN_VIBRATION", true))
+            vibrator.vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE));
     }
 
     public void goToQuizzTip(View v) {
         Toast viewToast = Toast.makeText(this, "Répondez aux divers calculs successifs posés dans le Quizz", Toast.LENGTH_LONG);
         viewToast.show();
+        if (preferences.getBoolean("SHARED_PREF_MAIN_VIBRATION", true))
+            vibrator.vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE));
     }
 
     public void goToLazerTip(View v) {
         Toast viewToast = Toast.makeText(this, "Répondez aux calculs le plus rapidement possible pour remporter le duel", Toast.LENGTH_LONG);
         viewToast.show();
+        if (preferences.getBoolean("SHARED_PREF_MAIN_VIBRATION", true))
+            vibrator.vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE));
     }
 
     public void goToMeteorTip(View v) {
         Toast viewToast = Toast.makeText(this, "Interceptez les météorites correctes pour résoudre le calcul proposé", Toast.LENGTH_LONG);
         viewToast.show();
+        if (preferences.getBoolean("SHARED_PREF_MAIN_VIBRATION", true))
+            vibrator.vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE));
     }
 }
--- app/src/main/java/fr/ensisa/minimaths/lazerbattle/LazerBattleMenu.java
+++ app/src/main/java/fr/ensisa/minimaths/lazerbattle/LazerBattleMenu.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import fr.ensisa.minimaths.Constantes;
 import fr.ensisa.minimaths.MainActivity;
@@ -18,24 +22,53 @@ import fr.ensisa.minimaths.Settings;
 public class LazerBattleMenu extends AppCompatActivity {
 
     private Animation animSlideIn4, animZoomIn12, animZoomIn13, animZoomIn14;
+    private Vibrator vibrator;
     private CardView header4;
     private RelativeLayout btn12, btn13, btn14;
+    private SharedPreferences preferences;
+    private SharedPreferences.Editor editor;
+    private TextView ms_facile, np_facile, nv_facile, ms_moyen, np_moyen, nv_moyen, ms_difficile, np_difficile, nv_difficile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lazer_battle_menu);
+
+        preferences = getSharedPreferences("SHARED_PREF_MAIN", MODE_PRIVATE);
+        editor = preferences.edit();
+        vibrator = (Vibrator) this.getSystemService(VIBRATOR_SERVICE);
+
         header4 = findViewById(R.id.header_lazermenu);
         btn12 = findViewById(R.id.lazer_facile);
         btn13 = findViewById(R.id.lazer_moyen);
         btn14 = findViewById(R.id.lazer_difficile);
+        ms_facile = findViewById(R.id.lazer_meilleurScore_facile);
+        ms_moyen = findViewById(R.id.lazer_meilleurScore_moyen);
+        ms_difficile = findViewById(R.id.lazer_meilleurScore_difficile);
+        np_facile = findViewById(R.id.lazer_nbParties_facile);
+        np_moyen = findViewById(R.id.lazer_nbParties_moyen);
+        np_difficile = findViewById(R.id.lazer_nbParties_difficile);
+        nv_facile = findViewById(R.id.lazer_nbVictoires_facile);
+        nv_moyen = findViewById(R.id.lazer_nbVictoires_moyen);
+        nv_difficile = findViewById(R.id.lazer_nbVictoires_difficile);
+
+        ms_facile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_MS_FACILE", 0)));
+        ms_moyen.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_MS_MOYEN", 0)));
+        ms_difficile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_MS_DIFFICILE", 0)));
+        np_facile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_NP_FACILE", 0)));
+        np_moyen.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_NP_MOYEN", 0)));
+        np_difficile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_NP_DIFFICILE", 0)));
+        nv_facile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_NV_FACILE", 0)));
+        nv_moyen.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_NV_MOYEN", 0)));
+        nv_difficile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_NV_DIFFICILE", 0)));
+
         animSlideIn4 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_down);
-        animSlideIn4.setStartOffset(300);
         animZoomIn12 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
-        animZoomIn12.setStartOffset(600);
         animZoomIn13 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
-        animZoomIn13.setStartOffset(800);
         animZoomIn14 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
+        animSlideIn4.setStartOffset(300);
+        animZoomIn12.setStartOffset(600);
+        animZoomIn13.setStartOffset(800);
         animZoomIn14.setStartOffset(1000);
         header4.startAnimation(animSlideIn4);
         btn12.startAnimation(animZoomIn12);
@@ -47,31 +80,43 @@ public class LazerBattleMenu extends AppCompatActivity {
         Intent intent = new Intent(this, LazerBattle.class);
         intent.putExtra(Constantes.ID_DIFFICULTY_NAME_EXTRAS, Constantes.ID_DIFFICULTY_FACILE);
         startActivity(intent);
+        if (preferences.getBoolean("SHARED_PREF_MAIN_VIBRATION", true))
+            vibrator.vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE));
     }
 
     public void goOnMedium(View v){
         Intent intent = new Intent(this, LazerBattle.class);
         intent.putExtra(Constantes.ID_DIFFICULTY_NAME_EXTRAS, Constantes.ID_DIFFICULTY_MEDIUM);
         startActivity(intent);
+        if (preferences.getBoolean("SHARED_PREF_MAIN_VIBRATION", true))
+            vibrator.vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE));
     }
     public void goOnHard(View v){
         Intent intent = new Intent(this, LazerBattle.class);
         intent.putExtra(Constantes.ID_DIFFICULTY_NAME_EXTRAS, Constantes.ID_DIFFICULTY_DIFFICILE);
         startActivity(intent);
+        if (preferences.getBoolean("SHARED_PREF_MAIN_VIBRATION", true))
+            vibrator.vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE));
     }
 
     public void backButton(View v){
         onBackPressed();
+        if (preferences.getBoolean("SHARED_PREF_MAIN_VIBRATION", true))
+            vibrator.vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE));
     }
 
     public void goToHome(View v){
         Intent home = new Intent(this, MainActivity.class);
         startActivity(home);
         overridePendingTransition(0, android.R.anim.slide_out_right);
+        if (preferences.getBoolean("SHARED_PREF_MAIN_VIBRATION", true))
+            vibrator.vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE));
     }
 
     public void goToSettings(View v){
         Intent settingsIntent = new Intent(this, Settings.class);
         startActivity(settingsIntent);
+        if (preferences.getBoolean("SHARED_PREF_MAIN_VIBRATION", true))
+            vibrator.vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE));
     }
 }
