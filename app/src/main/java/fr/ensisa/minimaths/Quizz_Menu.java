63a82441d4fc98272d755abe7c282def5aa062a3
Bastien Cron <flavicron@gmail.com>
2022-06-15 11:04:31 +0200

    mise à jour menu quiz et lazer

--- app/src/main/java/fr/ensisa/minimaths/Quizz_Menu.java
+++ app/src/main/java/fr/ensisa/minimaths/Quizz_Menu.java
@@ -18,13 +18,13 @@ import fr.ensisa.minimaths.lazerbattle.LazerBattle;
 
 public class Quizz_Menu extends AppCompatActivity {
 
-    private Animation animSlideIn3, animZoomIn9, animZoomIn10, animZoomIn11;
+    private Animation animSlideIn3, animZoomIn9, animZoomIn10, animZoomIn11, animZoomIn111;
     private CardView header3;
-    private RelativeLayout btn9, btn10, btn11;
+    private RelativeLayout btn9, btn10, btn11, btn111;
     private SharedPreferences preferences;
     private SharedPreferences.Editor editor;
     private Vibrator vibrator;
-    private TextView ms_facile, np_facile, ms_moyen, np_moyen, ms_difficile, np_difficile;
+    private TextView ms_facile, np_facile, ms_moyen, np_moyen, ms_difficile, np_difficile, ms_auto, np_auto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,34 +39,42 @@ public class Quizz_Menu extends AppCompatActivity {
         btn9 = findViewById(R.id.quiz_facile);
         btn10 = findViewById(R.id.quiz_moyen);
         btn11 = findViewById(R.id.quiz_difficile);
+        btn111 = findViewById(R.id.quiz_auto);
         ms_facile = findViewById(R.id.quiz_meilleurScore_facile);
         ms_moyen = findViewById(R.id.quiz_meilleurScore_moyen);
         ms_difficile = findViewById(R.id.quiz_meilleurScore_difficile);
+        ms_auto = findViewById(R.id.quiz_meilleurScore_auto);
         np_facile = findViewById(R.id.quiz_nbParties_facile);
         np_moyen = findViewById(R.id.quiz_nbParties_moyen);
         np_difficile = findViewById(R.id.quiz_nbParties_difficile);
+        np_auto = findViewById(R.id.quiz_nbParties_auto);
 
         ms_facile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_QUIZ_MS_FACILE", 0)));
         ms_moyen.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_QUIZ_MS_MOYEN", 0)));
         ms_difficile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_QUIZ_MS_DIFFICILE", 0)));
+        ms_auto.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_QUIZ_MS_AUTO", 0)));
         np_facile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_QUIZ_NP_FACILE", 0)));
         np_moyen.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_QUIZ_NP_MOYEN", 0)));
         np_difficile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_QUIZ_NP_DIFFICILE", 0)));
+        np_auto.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_QUIZ_NP_AUTO", 0)));
 
         animSlideIn3 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_down);
         animZoomIn9 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
         animZoomIn10 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
         animZoomIn11 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
+        animZoomIn111 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
 
         animSlideIn3.setStartOffset(300);
         animZoomIn9.setStartOffset(600);
         animZoomIn10.setStartOffset(800);
         animZoomIn11.setStartOffset(1000);
+        animZoomIn111.setStartOffset(1200);
 
         header3.startAnimation(animSlideIn3);
         btn9.startAnimation(animZoomIn9);
         btn10.startAnimation(animZoomIn10);
         btn11.startAnimation(animZoomIn11);
+        btn111.startAnimation(animZoomIn111);
     }
 
     @Override
@@ -76,9 +84,11 @@ public class Quizz_Menu extends AppCompatActivity {
         ms_facile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_QUIZ_MS_FACILE", 0)));
         ms_moyen.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_QUIZ_MS_MOYEN", 0)));
         ms_difficile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_QUIZ_MS_DIFFICILE", 0)));
+        ms_auto.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_QUIZ_MS_AUTO", 0)));
         np_facile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_QUIZ_NP_FACILE", 0)));
         np_moyen.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_QUIZ_NP_MOYEN", 0)));
         np_difficile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_QUIZ_NP_DIFFICILE", 0)));
+        np_auto.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_QUIZ_NP_AUTO", 0)));
     }
 
     public void goQuizzOnEasy(View v){
@@ -120,6 +130,8 @@ public class Quizz_Menu extends AppCompatActivity {
         startActivity(intent);
         if (preferences.getBoolean("SHARED_PREF_MAIN_VIBRATION", true))
             vibrator.vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE));
+        editor.putInt("SHARED_PREF_MAIN_QUIZ_NP_AUTO",preferences.getInt("SHARED_PREF_MAIN_QUIZ_NP_AUTO", 0)+1);
+        editor.apply();
     }
 
     public void backButton(View v){
--- app/src/main/java/fr/ensisa/minimaths/lazerbattle/LazerBattleMenu.java
+++ app/src/main/java/fr/ensisa/minimaths/lazerbattle/LazerBattleMenu.java
@@ -22,13 +22,13 @@ import fr.ensisa.minimaths.Settings;
 
 public class LazerBattleMenu extends AppCompatActivity {
 
-    private Animation animSlideIn4, animZoomIn12, animZoomIn13, animZoomIn14;
+    private Animation animSlideIn4, animZoomIn12, animZoomIn13, animZoomIn14, animZoomIn15;
     private Vibrator vibrator;
     private CardView header4;
-    private RelativeLayout btn12, btn13, btn14;
+    private RelativeLayout btn12, btn13, btn14, btn15;
     private SharedPreferences preferences;
     private SharedPreferences.Editor editor;
-    private TextView ms_facile, np_facile, nv_facile, ms_moyen, np_moyen, nv_moyen, ms_difficile, np_difficile, nv_difficile;
+    private TextView ms_facile, np_facile, nv_facile, ms_moyen, np_moyen, nv_moyen, ms_difficile, np_difficile, nv_difficile, ms_auto, np_auto, nv_auto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,38 +43,48 @@ public class LazerBattleMenu extends AppCompatActivity {
         btn12 = findViewById(R.id.lazer_facile);
         btn13 = findViewById(R.id.lazer_moyen);
         btn14 = findViewById(R.id.lazer_difficile);
+        btn15 = findViewById(R.id.lazer_auto);
         ms_facile = findViewById(R.id.lazer_meilleurScore_facile);
         ms_moyen = findViewById(R.id.lazer_meilleurScore_moyen);
         ms_difficile = findViewById(R.id.lazer_meilleurScore_difficile);
+        ms_auto = findViewById(R.id.lazer_meilleurScore_auto);
         np_facile = findViewById(R.id.lazer_nbParties_facile);
         np_moyen = findViewById(R.id.lazer_nbParties_moyen);
         np_difficile = findViewById(R.id.lazer_nbParties_difficile);
+        np_auto = findViewById(R.id.lazer_nbParties_auto);
         nv_facile = findViewById(R.id.lazer_nbVictoires_facile);
         nv_moyen = findViewById(R.id.lazer_nbVictoires_moyen);
         nv_difficile = findViewById(R.id.lazer_nbVictoires_difficile);
+        nv_auto = findViewById(R.id.lazer_nbVictoires_auto);
 
         ms_facile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_MS_FACILE", 0)));
         ms_moyen.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_MS_MOYEN", 0)));
         ms_difficile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_MS_DIFFICILE", 0)));
+        ms_auto.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_MS_AUTO", 0)));
         np_facile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_NP_FACILE", 0)));
         np_moyen.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_NP_MOYEN", 0)));
         np_difficile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_NP_DIFFICILE", 0)));
+        np_auto.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_NP_AUTO", 0)));
         nv_facile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_NV_FACILE", 0)));
         nv_moyen.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_NV_MOYEN", 0)));
         nv_difficile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_NV_DIFFICILE", 0)));
+        nv_auto.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_NV_AUTO", 0)));
 
         animSlideIn4 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.slide_down);
         animZoomIn12 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
         animZoomIn13 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
         animZoomIn14 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
+        animZoomIn15 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom_in);
         animSlideIn4.setStartOffset(300);
         animZoomIn12.setStartOffset(600);
         animZoomIn13.setStartOffset(800);
         animZoomIn14.setStartOffset(1000);
+        animZoomIn15.setStartOffset(1200);
         header4.startAnimation(animSlideIn4);
         btn12.startAnimation(animZoomIn12);
         btn13.startAnimation(animZoomIn13);
         btn14.startAnimation(animZoomIn14);
+        btn15.startAnimation(animZoomIn15);
     }
 
     @Override
@@ -84,12 +94,15 @@ public class LazerBattleMenu extends AppCompatActivity {
         ms_facile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_MS_FACILE", 0)));
         ms_moyen.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_MS_MOYEN", 0)));
         ms_difficile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_MS_DIFFICILE", 0)));
+        ms_auto.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_MS_AUTO", 0)));
         np_facile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_NP_FACILE", 0)));
         np_moyen.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_NP_MOYEN", 0)));
         np_difficile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_NP_DIFFICILE", 0)));
+        np_auto.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_NP_AUTO", 0)));
         nv_facile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_NV_FACILE", 0)));
         nv_moyen.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_NV_MOYEN", 0)));
         nv_difficile.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_NV_DIFFICILE", 0)));
+        nv_auto.setText(Integer.toString(preferences.getInt("SHARED_PREF_MAIN_LAZER_NV_AUTO", 0)));
     }
 
     public void goOnEasy(View v){
@@ -131,6 +144,8 @@ public class LazerBattleMenu extends AppCompatActivity {
         startActivity(intent);
         if (preferences.getBoolean("SHARED_PREF_MAIN_VIBRATION", true))
             vibrator.vibrate(VibrationEffect.createOneShot(30, VibrationEffect.DEFAULT_AMPLITUDE));
+        editor.putInt("SHARED_PREF_MAIN_LAZER_NP_AUTO",preferences.getInt("SHARED_PREF_MAIN_LAZER_NP_AUTO", 0)+1);
+        editor.apply();
     }
 
     public void backButton(View v){
